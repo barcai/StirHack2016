5202f1c9681bc67d07260d30088a1f0400c72f94
Bence Karpati <s1345679@sms.ed.ac.uk>
2016-02-13 17:54:59 +0000

    Add invalid JSON check, fix some bugs

--- getJSON.py
+++ getJSON.py
@@ -30,38 +30,58 @@ def check_api(api_data, auth_key, user_name, user_password):
     params = api_data["params"]
     url_to_check = "http://dogfish.tech/api/" + endpoint + "/" + params
 
-    # Make a test call to the given API
+
     response = None
+    api_status = None
+
+    # Make a test call to the given API
     if(access == "always"):
         response = requests.get(url_to_check)
+        #response = requests.get(url_to_check + "?broken=1")
+        #print(response.text)
 
     elif(access == "auth"):
-        auth = "&" + auth_key
-        response = requests.get(url_to_check)
+        auth = "&auth=" + auth_key
+        #print(url_to_check + auth)
+        response = requests.get(url_to_check + auth)
+        # ONLY FOR TESTING PURPOSES
+        #response = requests.get(url_to_check + auth + "&broken=1")
 
     elif(access == "token"):
         #token = "login?user=" + user_name + "&password=" + user_password
         token = getToken(user_name, user_password)
         if(token[1] != None):
             response = requests.get(url_to_check + "&token=" + token[1])
+            # ONLY FOR TESTING PURPOSES
+            #response = requests.get(url_to_check + "&token=" + token[1] + "&broken=1")
+        else:
+            api_status = "Token error"
 
 
-    api_status = None
+    # Check the response status of the API
+    if(api_status != "Token error"):
+        #print(response.text)
+        if(response.status_code == 500):
+            api_status = "Error 500"
 
-    if(response.status_code == 500):
-        api_status = "Error 500"
+        elif(response.status_code == 404):
+            api_status = "Error 404"
 
-    elif(response.status_code == 404):
-        api_status = "Error 404"
+        elif(response.status_code == 200):
+            try:
+                response.json()
+                api_status = "OK"
+            except ValueError:
+                api_status = "Invalid JSON"
 
-    elif(response.status_code == 200):
-        api_status = "OK"
+
+
 
     return api_status
 
 def main():
     for api in checkAPIList():
-        print api
+        #print api
         api_status = check_api(api, "thaddow", "thaddow", "thaddow")
         print(api_status)
     #print(getToken('thaddow', 'thaddow'))
