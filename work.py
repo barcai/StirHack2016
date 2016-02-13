f4068943fb727169361d7c3d47d2e5b1bb35c1a6
Bence Karpati <s1345679@sms.ed.ac.uk>
2016-02-13 16:20:41 +0000

    Add check_api function to check availability of an API

--- work.py
+++ work.py
@@ -1,32 +1,60 @@
 import requests, json, sys
 
 def checkAPIList():
-	url = "http://dogfish.tech/api/apis"
-	r = requests.get(url)
-	
-	if r.status_code == "500":
-		print ("500 Internal Server Error")
-	elif r.status_code == "404":
-		print ("404 Not Found")
-	else:
-		parsed_json = r.json()
-		return parsed_json['data']
-	return
-	
+    url = "http://dogfish.tech/api/apis"
+    r = requests.get(url)
+
+    if r.status_code == "500":
+        print ("500 Internal Server Error")
+    elif r.status_code == "404":
+        print ("404 Not Found")
+    else:
+        parsed_json = r.json()
+        return parsed_json['data']
+    return
+
+
 def getToken(username, password):
-	url = "http://dogfish.tech/api/login"
-	loginInfo = {'user': username, 'password': password}
-	r = requests.get(url, params=loginInfo)
-	if r.status_code == requests.codes.ok:
-		parsed_json = r.json()
-		if len(parsed_json['data']) > 0:
-			return (r.status_code, parsed_json['data']['token'])
-	return (r.status_code, None)
-	
+    url = "http://dogfish.tech/api/login"
+    loginInfo = {'user': username, 'password': password}
+    r = requests.get(url, params=loginInfo)
+    if r.status_code == requests.codes.ok:
+        parsed_json = r.json()
+        if len(parsed_json['data']) > 0:
+            return (r.status_code, parsed_json['data']['token'])
+    return (r.status_code, None)
 
-	
+def check_api(api_data, auth_key, user_name, user_password):
+    endpoint = api_data["endpoint"]
+    access = api_data["access"]
+    params = api_data["params"]
+    url_to_check = "http://dogfish.tech/api/" + endpoint + "/" + params
 
-for api in checkAPIList():
-	print api
-print(getToken('thaddow', 'thaddow'))
+    response = None
+    if(access == "always"):
+        response = requests.get(url_to_check)
+
+    elif(access == "auth"):
+        auth = "&" + auth_key
+        response = requests.get(url_to_check)
+
+    elif(access == "token"):
+        #token = "login?user=" + user_name + "&password=" + user_password
+        token = getToken(user_name, user_password)
+        if(token[1] != None):
+            response = requests.get(url_to_check + "&token=" + token[1])
+
+    #else:
+        #return error
+    return response
+
+def main():
+    for api in checkAPIList():
+        print api
+        response = check_api(api, "thaddow", "thaddow", "thaddow")
+        print(type(response))
+    print(getToken('thaddow', 'thaddow'))
+
+if __name__ == "__main__":
+    main()
 
